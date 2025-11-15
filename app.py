d4d9f3ff1433c71ab8cf9919fedd3c4e1b76c59c
fillip husebø <felipehusebo@gmail.com>
2025-11-15 15:46:42 +0100

    displayed questions and choices

--- app.py
+++ app.py
@@ -13,6 +13,30 @@ except json.JSONDecodeError:
 
 # def main_menu():
 #
-# def question_loop():
+
+questions = data["games"][0]["questions"]
+
+def question_loop(questions):
+    score = 0
+    wrong_answers = []
+
+    for q in questions:
+        print("\n" + q["question"])
+        for i, answer in enumerate(q["content"], start=1):
+                print(f"{i}. {answer}")
+
+        user_input = input("\nType your answer (1-4): ")
+
+        if user_input.lower() in ("q", "quit"):
+            print("Quitting early...")
+            break
+
+        if user_input.isdigit() and int(user_input) - 1 == q["correct"]:
+            print("Correct!")
+            score += 1
+        else:
+            print("Wrong!")
+
+question_loop(questions)
 #
-# def result_summary():
+# def result_summary()
