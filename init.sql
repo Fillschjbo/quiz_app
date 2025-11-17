6e66927cb5d879666bfc4515671e751ee368448c
fillip husebø <felipehusebo@gmail.com>
2025-11-17 21:07:41 +0100

    created procedures

--- init.sql
+++ init.sql
@@ -1,8 +1,3 @@
-from dotenv import load_dotenv, dotenv_values
-
-load_dotenv()
-config = dotenv_values(".env")
-
 CREATE DATABASE IF NOT EXISTS quiz_db;
 USE quiz_db;
 
@@ -22,4 +17,24 @@ CREATE TABLE IF NOT EXISTS questions (
     FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
 );
 
+
+
 # stored procedures
+
+DELIMITER //
+CREATE PROCEDURE get_games()
+    BEGIN
+        SELECT games.id, COUNT(questions.id) AS question_count
+        FROM games
+        LEFT JOIN questions ON games.id = questions.game_id
+        GROUP BY games.id
+        ORDER BY games.id;
+    END //
+
+CREATE PROCEDURE GetQuestionsForGame(IN gameId INT)
+    BEGIN
+        SELECT question_text, option_a, option_b, option_c, option_d, correct_index
+        FROM questions
+        WHERE game_id = gameId;
+    END //
+DELIMITER ;
